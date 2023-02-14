7f3fe89eacb2355eebcceb225de08ff7723de41c
Francisco Soto <francisco.stht@gmail.com>
2023-02-14 15:19:25 -0300

    Commit 2-14

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -18,19 +18,46 @@ app.get("/api/products", (req, res) => {
   res.json(newProducts);
 });
 
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
+
 app.get(`/api/products/:productID`, (req, res) => {
   console.log(req.params.productID);
   const singleProduct = products.find(
     (product) => product.id === Number(req.params.productID)
   );
   if (!singleProduct) {
-  return res.status(404).send('Product does not existe')
+    return res.status(404).send("Product does not existe");
   }
   res.json(singleProduct);
 });
 
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProduct = [...products];
+
+  if (search) {
+    sortedProduct = sortedProduct.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProduct = sortedProduct.slice(0, Number(limit));
+  }
+
+  if (sortedProduct.length < 1) {
+     return res.status(200).json({ success: true ,data:[]});
+  }
+
+   res.status(200).json(sortedProduct);
+  // res.send('hello')
+});
+
 app.get("*", (req, res) => {
-  res.status(404).send("resoruce not found");
+  res.status(404).send("resource not found");
 });
 
 app.listen(4000, () => {
